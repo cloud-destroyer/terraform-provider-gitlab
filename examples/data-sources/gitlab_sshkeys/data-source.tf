436f9507878acc5579f17399beaab40e9acbdb26
Mikael Fridh <mfridh@ea.com>
2022-11-24 03:13:40 +0100

    simplify ssh_keys examples for docs purposes - refs #1298

--- examples/data-sources/gitlab_sshkeys/data-source.tf
+++ examples/data-sources/gitlab_sshkeys/data-source.tf
@@ -1,6 +1,2 @@
 data "gitlab_sshkeys" "current_user" {
 }
-
-output "sshkeys" {
-  value = data.gitlab_sshkeys.current_user
-}
--- examples/resources/gitlab_sshkey/resource.tf
+++ examples/resources/gitlab_sshkey/resource.tf
@@ -1,14 +1,4 @@
-provider "gitlab" {
-  alias    = "not_an_admin"
-  base_url = "https://gitlab.example.com/api/v4/"
-  token    = data.aws_ssm_parameter.gitlab_token_not_an_admin.value
-  # token = "gl-some-token-2345652"
-}
-
-// Can only manage "own" keys.
-// Explicit alias of provider is not required. Merely an example.
 resource "gitlab_sshkey" "example" {
-  provider   = gitlab.not_an_admin
   title      = "example-key"
   key        = "ssh-rsa AAAA..."
   expires_at = "2016-01-21T00:00:00.000Z"
